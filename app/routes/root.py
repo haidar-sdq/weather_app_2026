e46c672215c973440c1321e6d0247256ba94546d
haidar-sdq <hadjk08@gmail.com>
2026-05-14 13:25:11 +0530

    Add health endpoint

--- app/routes/root.py
+++ app/routes/root.py
@@ -8,3 +8,10 @@ def home():
         "message": "Weather API running",
         "version": "v1"
     }, 200
+
+@root_bp.route("/health", methods=["GET"])
+def health():
+    return {
+        "status": "healthy",
+        "service": "weather-api"
+    }, 200
